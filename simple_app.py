43aaa164d3d9a4258f12724955abb437913213da
josediaz363 <josediaz363@pm.me>
2025-05-20 13:43:12 -0500

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World! Magellan EV Tracker coming soon."
+def index():
+    # We'll start by rendering a simple template
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
