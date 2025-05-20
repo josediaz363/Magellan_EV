47ae9c50f6e13cedd685476211b49d9cf88e824d
josediaz363 <josediaz363@pm.me>
2025-05-20 13:26:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
-from __init__ import create_app  # Changed from relative to absolute import
+from app import create_app
+
 app = create_app()
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5002))
     app.run(host='0.0.0.0', port=port, debug=False)
