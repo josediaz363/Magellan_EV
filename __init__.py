fecec7e92d0f517315e9461cee702b2ad881778d
josediaz363 <josediaz363@pm.me>
2025-05-20 13:19:21 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,8 +16,8 @@ def create_app():
     db.init_app(app)
     
     # Register blueprints
-    from .routes import main_bp
-    from .routes_reports import reports_bp
+    from routes import main_bp
+    from routes_reports import reports_bp
     
     app.register_blueprint(main_bp)
     app.register_blueprint(reports_bp)
