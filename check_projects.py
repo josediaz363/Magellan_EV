27efea0228cfc4faacefe6a6404965a99b2fa396
josediaz363 <josediaz363@pm.me>
2025-05-29 11:21:28 -0500

    Delete check_projects.py

--- check_projects.py
+++
@@ -1,9 +0,0 @@
-from models import db, Project
-from app import create_app
-
-app = create_app()
-with app.app_context():
-    projects = Project.query.all()
-    print(f'Number of projects: {len(projects)}')
-    for p in projects:
-        print(f'Project ID: {p.id}, Name: {p.name}')
