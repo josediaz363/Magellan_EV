f6c1952a994849c49e1efd65101cc5aae7197a99
josediaz363 <josediaz363@pm.me>
2025-05-31 18:17:17 -0500

    Delete services directory

--- services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Services package for Magellan EV Tracker v3.0
-"""
-from .project_service import ProjectService
-from .sub_job_service import SubJobService
-from .cost_code_service import CostCodeService
-from .work_item_service import WorkItemService
-from .rule_of_credit_service import RuleOfCreditService
